88d02228e53ba490e155ee3ba71025d8d545a6a9
MavFeuille <mavis.feuille@gmail.com>
2021-09-28 16:50:25 -0400

    Fix props name from {props.spot} -> {props.spots} on line14 in DayListItem.js to render the remaining spots properly

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -11,7 +11,7 @@ export default function DayListItem(props) {
   return (
     <li className="day-list__item" onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spot}</h3>
+      <h3 className="text--light">{props.spots}</h3>
     </li>
   );
 }
