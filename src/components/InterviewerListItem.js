89a9486256e9ace7ed11da989ce4d01352d7af14
MavFeuille <mavis.feuille@gmail.com>
2021-09-28 23:37:14 -0400

    Refactoring the InterviewerListItem.js by using the values of props in code

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "components/InterviewerListItem.scss";
-;const classNames = require('classnames');
+const classNames = require('classnames');
+
 
 export default function InterviewerListItem(props) {
+  const interviewerClass = classNames (
+    "interviewers__item", {
+    "interviewers__item--selected" :props.selected,
+  });
   
   return (
-    <li className="interviewers__item">
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.name)}>
       <img
-        className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        className={"interviewers__item-image"}
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.selected && props.name}
     </li>
   );
 }
+
--- stories/index.js
+++ stories/index.js
@@ -7,8 +7,10 @@ import "index.scss";
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
+import InterviewerListItem from "components/InterviewerListItem";
 
 
+// Stories of Button
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -36,6 +38,8 @@ storiesOf("DayListItem", module)
   <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
 ));
 
+
+// Stories of DayList
 const days = [
   {
     id: 1,
@@ -63,4 +67,40 @@ storiesOf("DayList", module)
   ))
   .add("Tuesday", () => (
     <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+  ));
+
+
+// Stories of InterviewerListItem
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+
+storiesOf("InterviewerListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+    />
+  ))
+  .add("Selected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected
+    />
+  ))
+  .add("Clickable", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={action("setInterviewer")}
+    />
   ));
