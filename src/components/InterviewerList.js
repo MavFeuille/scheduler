4eef1cca780f7cc526358a7d62d9af0cb6df4bd9
MavFeuille <mavis.feuille@gmail.com>
2021-09-29 12:35:33 -0400

    Update InterviewerList.js: add {interviewers} in line 23 in the <'interviewer__list'> element so that the list would render properly in hoziontal way

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -19,8 +19,8 @@ export default function InterviewerList(props) {
 
   return (
     <section className="interviewers">
-      <h4 className="interviewers__header text--light">{interviewers}</h4>
-      <ul className="interviewers__list"></ul>
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewers}</ul>
     </section>
   );
 }
