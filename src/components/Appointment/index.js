9d5b17103722bf15d2b035bacb44854bfc4c48b8
MavFeuille <mavis.feuille@gmail.com>
2021-09-29 13:39:40 -0400

    Adding all Appointment styles to src/components/Appointment/styles.scss

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "style.scss";
+
+
+
+export default function Appointment(props) {
+  
+  return (
+    <article className="appointment"></article>
+  );
+}
