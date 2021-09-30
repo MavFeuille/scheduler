787da860df5776e02f6ed5b92db19631f75de089
MavFeuille <mavis.feuille@gmail.com>
2021-09-29 23:50:16 -0400

    Add 3 more appointments to test data at the top of src/components/Applications.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,54 @@ import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Jon Snow",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Audry Storme",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+
+
 const days = [
   {
     id: 1,
