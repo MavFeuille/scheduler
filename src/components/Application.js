d1f9db8560fdba09009a848e3f9811ac3c8773b5
MavFeuille <mavis.feuille@gmail.com>
2021-10-01 17:45:20 -0400

    Add interviewers to the state object and Promise.all to fetch Interviewers Data

--- src/components/Application.js
+++ src/components/Application.js
@@ -58,7 +58,8 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = day => setState({ ...state, day });
@@ -80,6 +81,7 @@ export default function Application(props) {
       }))
    });
   }, [])
+  // console.log("======>Interviewers : ", state.interviewers)
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
