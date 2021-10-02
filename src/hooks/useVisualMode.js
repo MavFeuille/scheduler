723c4f30b1698dc573328bada9c3c10167823be2
MavFeuille <mavis.feuille@gmail.com>
2021-10-02 13:54:11 -0400

    Update function: Transition by adding 'replace = false' as new argument to decide transition behavior

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,36 +5,27 @@ export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
 
   //Transit state to a new mode
-  const transition = (newMode) => {
+  const transition = (newMode, replace = false) => {
     
     setMode(newMode);
     // console.log(">>>>newMode: ", newMode);
 
-    const newHistory = [...history];
-    // console.log("=== original newHistory === :", newHistory);
-    newHistory.push(newMode);
-    setHistory(newHistory);
-    console.log("=== newHistory === :", newHistory);
-    
-    
+    if (!replace) {
+      const newHistory = [...history];
+      // console.log("=== original newHistory === :", newHistory);
+      newHistory.push(newMode);
+      setHistory(newHistory);
+      console.log("=== newHistory === :", newHistory);
+    }
   }
 
   // Revert back to previous mode but not go over initial mode
   const back = () => {
-    
     if (history.length > 1) {
       history.pop();
       setMode(history[history.length - 1]);
       // console.log("----> history: ", history)
     }
-    
-    
-    
   }
-
   return { mode, transition, back };
-
 }
-
-
-   
