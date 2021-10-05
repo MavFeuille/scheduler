002dc53927ca7bd21424c4533de6d1f2d122fd71
MavFeuille <mavis.feuille@gmail.com>
2021-10-05 00:59:49 -0400

    Create useApplicationData.js to fulfill separation of concerns, Remove all unnecessary codes from Application.js to useApplicationData.js to fulfill separation of concerns

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,141 +1,28 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from "react";
+import useApplicationData from "hooks/useApplicationData";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time: "2pm",
-//     interview: {
-//       student: "Jon Snow",
-//       interviewer: {
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Audry Storme",
-//       interviewer: {
-//         id: 3,
-//         name: "Mildred Nazir",
-//         avatar: "https://i.imgur.com/T2WwVfS.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 5,
-//     time: "4pm",
-//   }
-// ];
-
 
 export default function Application(props) {
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-
-  const setDay = day => setState({ ...state, day });
-  // const setDays = days => setState(prev => ({ ...prev, days }));
-
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers")
-    ]).then(response => {
-      // console.log(response.data);
-      setState(
-        prev => ({
-        ...prev,
-        // ({
-        days: response[0].data,
-        appointments: response[1].data,
-        interviewers: response[2].data
-      }))
-   });
-  }, [setState])
-  // console.log("======>Interviewers : ", state.interviewers)
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
   
   const appointments = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state, state.day);
   
-  
-  
-  // const dailyAppointments = [];
   const schedule = appointments.map(appointment => {
+    
     const interview = getInterview(state, appointment.interview);
     
-   const bookInterview = (id, interview) => {
-      console.log(id, interview);
-      
-      const appointment = {
-        ...state.appointments[id],
-        interview: { ...interview }
-      };
-
-      const appointments = {
-        ...state.appointments,
-        [id]: appointment
-      };
-
-      return axios.put(`/api/appointments/${id}`, {interview})
-        .then((res) => {
-          setState({
-          ...state,
-          appointments
-        });
-      })
-    }
-
-    const cancelInterview = (id) => {
-      const appointment = {
-        ...state.appointments[id],
-        interview: null
-      };
-
-      const appointments = {
-        ...state.appointments,
-        [id]: appointment
-      };
-
-      return axios.delete(`/api/appointments/${id}`)
-        .then((res) => {
-          setState({
-          ...state,
-          appointments
-        });
-      })
-    }
- 
     return (  
       <Appointment 
         key={appointment.id}
@@ -148,9 +35,7 @@ export default function Application(props) {
       />
     )
   });
-  // console.log("======> schedule: ", schedule)
-
-
+ 
   return (
     <main className="layout">
       <section className="sidebar">
@@ -165,7 +50,6 @@ export default function Application(props) {
           days={state.days}
           day={state.day}
           setDay={setDay}
-          // day => console.log(day)
         />
         </nav>
         <img
